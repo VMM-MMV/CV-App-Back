e47d11fc25d3baf55a8207a5ab87a3e9393275b7
VMM-MMV <121231490+VMM-MMV@users.noreply.github.com>
2023-12-04 14:35:36 +0200

    allowed all origins for test

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -15,10 +15,11 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:3000")
+                        .allowedOrigins("*") // Allow all origins
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                         .allowedHeaders("*");
             }
         };
     }
 }
+
